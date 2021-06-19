40f2fefa1e4a07a938270867cdff4f2465fe8f2e
Rishav7 <rajrishav381.rr@gmail.com>
2021-06-19 12:13:52 +0530

    update
    Signed-off-by: Rishav7 <rajrishav381.rr@gmail.com>

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema({
     },
     photo: {
         type: String,
-        default: "pic.jpg"
+        default:"pic.jpg"
     },
     isAdmin: {
         type: Boolean,
