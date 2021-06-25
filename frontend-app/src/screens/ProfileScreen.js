2cae560f1b314b923c1e7a4a6a5292d2fa52bb9f
my name <me@xyz.com>
2021-06-25 11:06:06 +0530

    swarnim test 2

--- frontend-app/src/screens/ProfileScreen.js
+++ frontend-app/src/screens/ProfileScreen.js
@@ -3,7 +3,7 @@ import { Row, Col, Form, Button, Figure, Image } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 
 /*Component imports*/
-// rishav test
+// rishav testing imports
 
 import Message from '../components/Message'
 import Loader from '../components/Loader'
